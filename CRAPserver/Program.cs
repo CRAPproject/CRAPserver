dacd7af905410e43a70e863f448118dab66403b2
ThePhilStrongProject <philip.a.strong@gmail.com>
2015-10-01 21:14:42 +0100

    Added simple HTTP server

--- CRAPserver/Program.cs
+++ CRAPserver/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+
 
 namespace CRAPserver
 {
@@ -14,9 +19,40 @@ namespace CRAPserver
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+            HttpListener server = new HttpListener();
+            server.Prefixes.Add("http://127.0.0.1/");
+            server.Prefixes.Add("http://localhost/");
+
+            server.Start();
+
+            Debug.WriteLine("Listening...");
+
+            while (true)
+            {
+                HttpListenerContext context = server.GetContext();
+                HttpListenerResponse response = context.Response;
+
+                string page = Directory.GetCurrentDirectory() + context.Request.Url.LocalPath;
+
+                if (page == string.Empty)
+                    page = "index.html";
+
+                TextReader tr = new StreamReader(page);
+                string msg = tr.ReadToEnd();
+
+
+                byte[] buffer = Encoding.UTF8.GetBytes(msg);
+
+                response.ContentLength64 = buffer.Length;
+                Stream st = response.OutputStream;
+                st.Write(buffer, 0, buffer.Length);
+
+                context.Response.Close();
+            }
         }
     }
 }
