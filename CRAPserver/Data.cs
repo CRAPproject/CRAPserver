68e842b71f42771f0d1466f200be61167b3f4bae
tomganleylee <tomganleylee@gmail.com>
2015-10-03 00:07:25 +0100

    added CSV save/Load +started searching Get setters

--- CRAPserver/Data.cs
+++ CRAPserver/Data.cs
@@ -39,8 +39,144 @@ namespace CRAPserver
         {
             DataTable table = new DataTable();
             table.Columns.Add("NodeID", typeof(int));
-            table.Columns.Add("Temperature", typeof(int));
+            table.Columns.Add("Temperature", typeof(string));
             return table;
         }
+
+        public string GetStateData(int Node, string Coloum)
+        {
+            string value;
+            DataRow[] Row;
+            Row = state.Select("NodeID = " + Node);
+           DataRow temp = Row[0];
+           value = temp[Coloum].ToString();
+            return value;
+        }
+        public void SetStateData(int Node, string Coloum, string value)
+    {
+        DataRow[] Row;
+        Row = state.Select("NodeID = " + Node);
+        DataRow temp = Row[0];
+        temp[Coloum] = value;
+    }
+
+        private bool saveNodeTable(string location, DataTable NodeTable)
+        {
+            // saves Node table to XML 
+            bool success;
+            try
+            {
+                NodeTable.WriteXml(location);
+                success = true;
+            }
+            catch
+            {
+                Console.WriteLine("Unable to save Node Table");
+                success = false;
+            }
+            return success;
+        }
+        private bool saveTypeTable(string location, DataTable NodeTable)
+        {
+            // saves Node table to XML 
+            bool success;
+            try
+            {
+                NodeTable.WriteXml(location);
+                success = true;
+            }
+            catch
+            {
+                Console.WriteLine("Unable to save Type Table");
+                success = false;
+            }
+            return success;
+        }
+        private bool saveStateTable(string location, DataTable NodeTable)
+        {
+            // saves State table to XML 
+            bool success;
+            try
+            {
+                NodeTable.WriteXml(location);
+                success = true;
+            }
+            catch
+            {
+                Console.WriteLine("Unable to save State Table");
+                success = false;
+            }
+            return success;
+        }
+        private DataTable LoadNodeTable(string location)
+        {
+            DataTable TempNodeTable = new DataTable();
+
+            try
+            {
+                TempNodeTable.ReadXml(location);
+            }
+            catch
+            {
+                Console.WriteLine("Restore of Node table fail");
+            }
+            return TempNodeTable;
+        }
+
+        private DataTable LoadTypesTable(string location)
+        {
+            DataTable TempTypesTable = new DataTable();
+
+            try
+            {
+                TempTypesTable.ReadXml(location);
+            }
+            catch
+            {
+                Console.WriteLine("Restore of Node table fail");
+            }
+            return TempTypesTable;
+        }
+
+        private DataTable LoadStateTable(string location)
+        {
+            DataTable TempStateTable = new DataTable();
+
+            try
+            {
+                TempStateTable.ReadXml(location);
+            }
+            catch
+            {
+                Console.WriteLine("Restore of Node table fail");
+            }
+            return TempStateTable;
+        }
+        public bool saveAll(string Folder)
+        {
+            ///<summary> Saves All tables into the Folder given </summary>
+            bool success;
+            try
+            {
+                saveNodeTable(Folder + "NodeData.csv", nodes);
+                saveStateTable(Folder + "StateData.csv", state);
+                saveTypeTable(Folder + "TypeData.csv", types);
+                success = true;
+            }
+            catch
+            {
+                success = false;
+            }
+            return success;
+        }
+        public bool LoadAll(string Folder)
+        {
+            nodes = LoadNodeTable(Folder + "NodeData.csv");
+            state = LoadStateTable(Folder + "StateData.csv");
+            types = LoadTypesTable(Folder + "TypeData.csv");
+            return true;
+        
+        }
+       
     }
 }
