c63638965a318c20889c89566821c86ccc2bb509
ThePhilStrongProject <philip.a.strong@gmail.com>
2015-10-04 15:57:44 +0100

    Built HTTP argument parser, changed HTTP port, removed 'Tom is gay'

---
+++ CRAPserver/HTTPGetArgumentParse.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CRAPserver
+{
+    class HTTPGetArgumentParse
+    {
+        string messageToBeParsed;
+
+        public HTTPGetArgumentParse(string message)
+        {
+            messageToBeParsed = message;
+        }
+
+        public int getNodeID()
+        {
+            return Convert.ToInt32(getParameter("nodeid"));
+        }
+
+        public int getType()
+        {
+            if (getParameter("type").Equals("request")) return 0;
+            else if (getParameter("type").Equals("command")) return 1;
+            else return -1;
+        }
+        
+        public string getParameter(string argument)
+        {
+            String returnString = "";
+            try
+            {
+                var queryString = messageToBeParsed.Substring(messageToBeParsed.IndexOf('?')).Split('#')[0];
+                var parsedString = HttpUtility.ParseQueryString(queryString);
+                returnString = parsedString.Get(argument);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return returnString;
+        }
+    }
+}
